c48b6e762d9b6d60c4bb894447ecd2aed2d16c7f
Vignesh waran <vw170996@gmail.com>
2021-02-27 11:24:37 +0530

    Updates

---
+++ precache-manifest.6d513b0fb03bad283f6d170e8a316422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6251f56a06a765672932",
+    "url": "/react-lights-out/static/css/main.bd68e122.chunk.css"
+  },
+  {
+    "revision": "6251f56a06a765672932",
+    "url": "/react-lights-out/static/js/main.6251f56a.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/react-lights-out/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "1764a79bcd54ed66a092",
+    "url": "/react-lights-out/static/js/runtime~main.1764a79b.js"
+  },
+  {
+    "revision": "b52281e1b9ef643e662c912ac4968dc1",
+    "url": "/react-lights-out/index.html"
+  }
+];
